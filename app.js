157237f2960c378b8fe6a69a4d125ab8978aa10b
alexsvgithub <53620960+alexsvgithub@users.noreply.github.com>
2020-05-03 23:52:12 +0530

    comments removed

--- app.js
+++ app.js
@@ -8,26 +8,6 @@ $(document).ready(function() {
   $('#about').click();
 });
 
-
-
-//   jQuery(function(){
-//     jQuery('.targetDiv').hide();
-//   });
-//
-//   $(function() {
-//   $('mybutton').focus();
-// });
-
-
-
-
-  // jQuery(function() {
-  //   jQuery('.targetDiv').hide();
-  //   jQuery('#hp').show();
-  // });
-    // jQuery('#showall').click(function() {
-    //   jQuery('.targetDiv').show();
-    // });
   jQuery('.showSingle').click(function() {
     jQuery('.targetDiv').hide();
     jQuery('#div' + $(this).attr('target')).show();
@@ -36,19 +16,6 @@ $(document).ready(function() {
   });
 });
 
-// jQuery(document).click(function(){
-//   if(evt.target.id == "menu_content")
-//   {
-//     return;
-//   }
-//
-//      //For descendants of menu_content being clicked, remove this check if you do not want to put constraint on descendants.
-//   else
-//   {
-//     $('.right').scrollTop(0);
-//   }
-//
-// });
 
 $('body').click(function(evt){
     if(!$(evt.target).is('#menu_content')) {
@@ -56,6 +23,7 @@ $('body').click(function(evt){
         $('.right').scrollTop(0);
     }
 });
+
 function image(){
   location.href='images/resumedorx.docx';
 }
